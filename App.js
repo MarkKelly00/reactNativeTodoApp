4f4732dd2f3a980db586626d5de5187297ec5baa
Mako3 <big.mako@gmail.com>
2021-03-29 02:53:31 -0700

    added todoItem and addTodo components with functionality working

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import Header from "./components/header/header";
+import TodoItem from "./components/todoItem/todoItem";
+import AddTodo from './components/addTodo/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -11,15 +13,32 @@ export default function App() {
     { text: "workout", key: "4" },
   ]);
 
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ];
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* to form */}
+        <AddTodo submitHandler={submitHandler}/>
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
           />
         </View>
       </View>
@@ -32,8 +51,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   content: {
     padding: 40,
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,25 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Header() {
-  return (
-    <View style={styles.header}>
-      <Text style={styles.title}>My Todos</Text>
-    </View>
-  );
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>My Todo List</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  header: {
-    height: "80px",
-    width: "100%",
-    paddingTop: "38px",
-    backgroundColor: "coral",
-  },
-  title: {
-    textAlign: "center",
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
+    header: {
+        height: 80,
+        paddingTop: 38,
+        backgroundColor: 'coral'
+    },
+    title: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+})
