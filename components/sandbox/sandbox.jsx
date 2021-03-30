6c51408c3517b48e7253aea5905378fac7bc46bf
Mako3 <big.mako@gmail.com>
2021-03-29 22:32:42 -0700

    adjusted styling to sandbox component to test flexbox

--- components/sandbox/sandbox.jsx
+++ components/sandbox/sandbox.jsx
@@ -14,7 +14,24 @@ export default function Sandbox() {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         paddingTop: 40,
         backgroundColor: '#ddd',
+    },
+    boxOne: {
+        backgroundColor: 'violet',
+        padding: 10,
+    },
+    boxTwo: {
+        backgroundColor: 'gold',
+        padding: 10,
+    },
+    boxThree: {
+        backgroundColor: 'coral',
+        padding: 10,
+    },
+    boxFour: {
+        backgroundColor: 'skyblue',
+        padding: 10,
     }
 });
