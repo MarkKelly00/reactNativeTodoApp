ee07ad0be5a8af6f2dd9fca7288377d80ed0a8e0
Mako3 <big.mako@gmail.com>
2021-03-29 22:57:08 -0700

    added trash icon to list, to delete item

--- components/todoItem/todoItem.jsx
+++ components/todoItem/todoItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function todoItem({ item, pressHandler }) {
 
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+            <MaterialIcons name="delete" size={24} color="#333" />
+            <Text style={styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
@@ -14,9 +18,14 @@ const styles = StyleSheet.create({
     item: {
         padding: 16,
         marginTop: 16,
+        flexDirection: 'row',
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius: 10
+        borderRadius: 10,
+    },
+    itemText: {
+        marginLeft: 10,
+        fontSize: 16,
     }
 })
